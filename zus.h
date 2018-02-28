26bcb828955b07858bc11b0530c14291f7f7f0bf
Shachar Sharon <sshachar@netapp.com>
2018-02-28 16:59:03 +0200

    zus: Define STATX_xxx constants if missing in <linux/stat.h>
    On older systems (e.g., CnetOS7) the STATX_xxx constants do not
    exist yet in the standard <linux/stat.h> header. Define them
    manually.

--- zus.h
+++ zus.h
@@ -20,6 +20,25 @@
 #include <linux/stat.h>
 #define __GLIBC__ SAVE___GLIBC__
 
+/* Have STATX_xxx defines if not exist in <linux/stat.h> (e.g., centos7.4) */
+#ifndef STATX_TYPE
+#define STATX_TYPE              0x00000001U     /* Want/got stx_mode & S_IFMT */
+#define STATX_MODE              0x00000002U     /* Want/got stx_mode & ~S_IFMT */
+#define STATX_NLINK             0x00000004U     /* Want/got stx_nlink */
+#define STATX_UID               0x00000008U     /* Want/got stx_uid */
+#define STATX_GID               0x00000010U     /* Want/got stx_gid */
+#define STATX_ATIME             0x00000020U     /* Want/got stx_atime */
+#define STATX_MTIME             0x00000040U     /* Want/got stx_mtime */
+#define STATX_CTIME             0x00000080U     /* Want/got stx_ctime */
+#define STATX_INO               0x00000100U     /* Want/got stx_ino */
+#define STATX_SIZE              0x00000200U     /* Want/got stx_size */
+#define STATX_BLOCKS            0x00000400U     /* Want/got stx_blocks */
+#define STATX_BASIC_STATS       0x000007ffU     /* The stuff in the normal stat struct */
+#define STATX_BTIME             0x00000800U     /* Want/got stx_btime */
+#define STATX_ALL               0x00000fffU     /* All currently supported flags */
+#define STATX__RESERVED         0x80000000U     /* Reserved for future struct statx expansion */
+#endif
+
 #include "zus_api.h"
 #include "_pr.h"
 #include "list.h"
